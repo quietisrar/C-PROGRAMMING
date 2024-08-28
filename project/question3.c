a049178ea8a15fce41ab43b185f1e08088160de9
Meghparekh5 <parekhmegh155@gmail.com>
2024-08-28 14:50:46 +0530

    add change

--- project/question3.c
+++ project/question3.c
@@ -1,5 +1,14 @@
-#include <stdbool.h>
+#include <stdio.h>
 void main()
 {
     int first_ang, second_ang, third_ang;
+
+    printf("enter the value of first_ang:-");
+    scanf("%d", &first_ang);
+
+    printf("enter the value of second_ang:-");
+    scanf("%d", &second_ang);
+
+    third_ang = 180 - (first_ang + second_ang);
+    printf("third angle: %d\n", third_ang);
 }
